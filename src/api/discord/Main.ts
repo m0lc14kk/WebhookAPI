2e74ffa53720b3db21db17f2fd50fe56a9f2eceb
m0lc14kk <m0lc14kk@outlook.com>
2024-03-09 09:14:29 +0100

    Dodano wartości podstawowych kolorów w enumie.
    Dodano wartości kolorów w Minecraftcie i specjalnego koloru do tworzenia przejrzystych embedów, dla uproszczenia ich tworzenia. Nadal jest możliwe ustawianie innego koloru embeda niż te z Minecrafta.

--- src/api/discord/Main.ts
+++ src/api/discord/Main.ts
@@ -1,2 +1,4 @@
 export { EmbedServer } from "./embed/Embed";
+export { EmbedColors } from "./embed/enum/EmbedColors";
+
 export { WebhookServer } from "./webhook/Webhook";
---
+++ src/api/discord/embed/enum/EmbedColors.ts
@@ -0,0 +1,22 @@
+enum EmbedColors {
+    DiscordDesktopClean = 3092790,
+
+    MinecraftColor0 = 0,
+    MinecraftColor1 = 170,
+    MinecraftColor2 = 43520,
+    MinecraftColor3 = 43690,
+    MinecraftColor4 = 11141120,
+    MinecraftColor5 = 11141290,
+    MinecraftColor6 = 16755200,
+    MinecraftColor7 = 11184810,
+    MinecraftColor8 = 5592405,
+    MinecraftColor9 = 5592575,
+    MinecraftColorA = 5635925,
+    MinecraftColorB = 5636095,
+    MinecraftColorC = 16733525,
+    MinecraftColorD = 16733695,
+    MinecraftColorE = 16777045,
+    MinecraftColorF = 16777215,
+};
+
+export { EmbedColors };
