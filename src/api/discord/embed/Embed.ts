aef8c0a4a4edf32a630309f370c995d443e7985c
m0lc14kk <m0lc14kk@outlook.com>
2024-03-09 09:22:53 +0100

    Udokumentowano kod.
    Udokumentowano klase EmbedServer oraz WebhookServer.

--- src/api/discord/embed/Embed.ts
+++ src/api/discord/embed/Embed.ts
@@ -3,21 +3,40 @@ class EmbedServer {
     private embedDescription: string | null;
     private embedColor: number;
 
+    /**
+     * Funkcja do ustawiania tytułu embeda.
+     * @param text Tytuł embeda. Ustawienie null zakończy się brakiem tytułu.
+     * @returns Zwraca element, by ułatwić szybkość pisania.
+     */
     public setTitle(text: string | null): EmbedServer {
         this.embedTitle = text;
         return this;
     };
 
+    /**
+     * Funkcja do ustawiania opisu embeda.
+     * @param text Opis embeda. Ustawienie null zakończy się brakiem opisu.
+     * @returns Zwraca element, by ułatwić szybkość pisania.
+     */
     public setDescription(text: string | null): EmbedServer {
         this.embedDescription = text;
         return this;
     };
 
+    /**
+     * Funkcja do ustawiania koloru embeda.
+     * @param color Kolor embeda. Można korzystać z palety EmbedColors.
+     * @returns Zwraca element, by ułatwić szybkość pisania.
+     */
     public setColor(color: number): EmbedServer {
         this.embedColor = color;
         return this;
     };
 
+    /**
+     * Funkcja od pobierania embeda w notacji JSON.
+     * @returns Zwraca obiekt JSON, gotowy do wysłania na kanał Discord.
+     */
     public getEmbed() {
         return {
             color: this.embedColor,
--- src/api/discord/webhook/Webhook.ts
+++ src/api/discord/webhook/Webhook.ts
@@ -5,16 +5,29 @@ class WebhookServer {
     private webhookURL: string;
     private webhookContent: IWebhookContent;
 
+    /**
+     * Funkcja od ustawiania linku URL webhooka.
+     * @param url URL webhooka, na którego kanał wyśle wiadomość.
+     * @returns Zwraca element, by ułatwić szybkość pisania.
+     */
     public setWebhookURL(url: string): WebhookServer {
         this.webhookURL = url;
         return this;
     };
 
+    /**
+     * Funkcja od ustawiania zawartości wiadomości webhooka.
+     * @param content Treść wiadomości.
+     * @returns Zwraca element, by ułatwić szybkość pisania.
+     */
     public setWebhookContent(content: IWebhookContent): WebhookServer {
         this.webhookContent = content;
         return this;
     };
 
+    /**
+     * Funkcja od wysyłania wiadomości webhooka na kanał. W przypadku BeforeEventów, należy przeczekać ticka.
+     */
     public sendWebhook(): void {
         const request = new HttpRequest(this.webhookURL)
             .setBody(JSON.stringify(
