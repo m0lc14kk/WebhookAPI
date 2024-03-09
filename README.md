b3ee69f250cf29c8ec142322c7b12ff20b89c095
m0lc14kk <m0lc14kk@outlook.com>
2024-03-09 09:44:35 +0100

    Dodano komentarze do kodu.
    Dodano komentarze do kodu w README.md i opisano działanie kodu w przykładowym listenerze.

--- README.md
+++ README.md
@@ -17,6 +17,7 @@
 import { system, world } from "@minecraft/server";
 import { WebhookServer } from "../../api/discord/webhook/Webhook";
 
+// Link należy zamienić ze swoim linkiem do webhooka.
 const webhookUrl = "https://discord.com/api/webhooks/1215756079837872228/Djthzxi4c3PHtv62ExOp7Qu_UaVhp8OzRHs-hkkAW50xDA2WlLLl6nLww5OP_Dzr8deo";
 
 world.beforeEvents.chatSend.subscribe((eventData) => {
@@ -31,6 +32,8 @@ world.beforeEvents.chatSend.subscribe((eventData) => {
     });
 });
 ```
+
+Dokładniejsza analizę znajdziesz w pliku <code>src/core/listeners/ChatSendBeforeEvent.ts</code>.
 
 <h3>
     Jak zacząć używać?
--- src/core/listeners/ChatSendBeforeEvent.ts
+++ src/core/listeners/ChatSendBeforeEvent.ts
@@ -1,17 +1,28 @@
+// Importowanie modułów.
 import { system, world } from "@minecraft/server";
 import { WebhookServer } from "../../api/discord/webhook/Webhook";
 
+// Link należy zamienić ze swoim linkiem do webhooka.
 const webhookUrl = "https://discord.com/api/webhooks/1215756079837872228/Djthzxi4c3PHtv62ExOp7Qu_UaVhp8OzRHs-hkkAW50xDA2WlLLl6nLww5OP_Dzr8deo";
 
+// Podłączanie listenera.
 world.beforeEvents.chatSend.subscribe((eventData) => {
+
+    // Bypassowanie ticka spowodowanego używaniem BeforeEventu.
     system.run(() => {
+
+        // Deklarowanie nowego webhooka.
         new WebhookServer()
+
+            // Ustawianie URL webhooka.
             .setWebhookURL(webhookUrl)
+
+            // Ustawianie treści wiadomości w webhooku.
             .setWebhookContent({
-                content: `test`,
+                content: "test",
             })
 
+            // Wysyła webhooka na kanał.
             .sendWebhook();
-
     });
 });
