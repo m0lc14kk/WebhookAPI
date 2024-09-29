a746200e9234d3075d8be6d44b6629fbeaf0d3f8
m0lc14kk <m0lc14kk@outlook.com>
2024-09-29 11:08:33 +0200

    feat: documented embed configuration
    Added documentation for EmbedConfiguration class.

--- src/api/embeds/EmbedConfiguration.ts
+++ src/api/embeds/EmbedConfiguration.ts
@@ -1,7 +1,16 @@
 import { EmbedContentTypes } from "./types/EmbedContentTypes";
 
+/**
+ * Class that allows set default properties for embeds.
+ * @private
+ */
 class EmbedConfiguration {
+    /**
+     * Default color of an embed.
+     */
     public static embedColor: number = 0x000000;
+    
+    private constructor() {};
 };
 
 export { EmbedConfiguration };
