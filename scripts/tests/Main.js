f361cefe2c0dc07675fbc82d047fd3b12107e183
m0lc14kk <m0lc14kk@outlook.com>
2024-09-30 07:13:31 +0200

    feat: changed some format
    Changed some format for example file.

--- scripts/tests/Main.js
+++ scripts/tests/Main.js
@@ -1,9 +1,13 @@
 /**
- * Sends a basic message via webhook.
- */
-import "./src/BasicMessage";
-/**
  * Basic configuration management.
+ * This file should be on-top others, to be sure, that configuration will be loaded before other scripts.
+ * @tutorial
  * @author m0lc14kk
  */
 import "./src/ChangeConfiguration";
+/**
+ * Sends a basic message via webhook.
+ * @tutorial
+ * @author m0lc14kk
+ */
+import "./src/BasicMessage";
--- src/tests/Main.ts
+++ src/tests/Main.ts
@@ -1,11 +1,14 @@
 /**
- * Sends a basic message via webhook.
- */
-
-import "./src/BasicMessage";
-
-/**
  * Basic configuration management. 
+ * This file should be on-top others, to be sure, that configuration will be loaded before other scripts.
+ * @tutorial
  * @author m0lc14kk
  */
 import "./src/ChangeConfiguration";
+
+/**
+ * Sends a basic message via webhook.
+ * @tutorial
+ * @author m0lc14kk
+ */
+import "./src/BasicMessage";
