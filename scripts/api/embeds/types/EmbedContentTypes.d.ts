b86c53e66bd35b76a5373b41c1c9073c2ec0e54f
m0lc14kk <m0lc14kk@outlook.com>
2024-11-18 14:29:56 +0100

    docs: some fixes in docs

--- scripts/api/embeds/types/EmbedContentTypes.d.ts
+++ scripts/api/embeds/types/EmbedContentTypes.d.ts
@@ -1,6 +1,8 @@
 /**
  * Types of a embed content.
  * `null` means, it won't be present in display.
+ *
+ * @default null
  */
 type EmbedContentTypes = string | null;
 export type { EmbedContentTypes };
