ec489a46109a276bc58e32eb2b35c0393065c812
m0lc14kk <m0lc14kk@outlook.com>
2024-11-24 11:06:48 +0100

    feat: added mobile navbar

--- src/components/layout/navbar/NavbarComponent.tsx
+++ src/components/layout/navbar/NavbarComponent.tsx
@@ -1,7 +1,9 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { NavbarLinkType } from "./types/NavbarLinkType";
+import { RxHamburgerMenu } from "react-icons/rx";
+import { IoCloseOutline } from "react-icons/io5";
 import NavbarLink from "./NavbarLink.";
 
 const navbarLinks: readonly NavbarLinkType[] = [
@@ -24,6 +26,12 @@ const navbarLinks: readonly NavbarLinkType[] = [
 ];
 
 const NavbarComponent = (): React.JSX.Element => {
+    const [navbarToggled, setNavbarToggled] = useState<boolean>(false);
+
+    const handleClick = (): void => {
+        setNavbarToggled(!navbarToggled);
+    };
+
     return (
         <>
             <nav className="bg-transparent absolute flex items-center justify-between px-8 lg:px-[14%] py-8 border-b-2 border-b-[#90909020] w-full backdrop-blur-2xl h-24">
@@ -45,7 +53,44 @@ const NavbarComponent = (): React.JSX.Element => {
                         />
                     )}
                 </ul>
+
+                <RxHamburgerMenu
+                    size={35}
+                    className="bg-transparent p-1 md:hidden"
+                    onClick={handleClick}
+                />
             </nav>
+
+            <aside className={`w-[100dvw] h-[100dvh] ${navbarToggled ? "left-0" : "left-[100%]"} fixed duration-150 bg-black/30 backdrop-blur-sm flex justify-end md:hidden z-10`}>
+                <div className="bg-transparent w-[30%] h-full" onClick={handleClick}>
+                
+                </div>
+                <nav className="bg-[#141025] w-[70%] h-full p-6">
+                    <header className="flex items-center justify-between bg-transparent">
+                        <img
+                            width={100}
+                            alt="/"
+                            src={`${process.env.PUBLIC_URL}/imgs/logo-smaller.png`}
+                            className="bg-transparent"
+                        />
+
+                        <IoCloseOutline
+                            size={35}
+                            className="bg-transparent p-1 md:hidden"
+                            onClick={handleClick}
+                        />
+                    </header>
+
+                    <ul className="bg-transparent flex flex-col pt-6 gap-y-2">
+                        {navbarLinks.map(({ name, path }) => 
+                            <NavbarLink 
+                                name={name} 
+                                path={path} 
+                            />
+                        )}
+                    </ul>
+                </nav>
+            </aside>
         </>
     );
 };
