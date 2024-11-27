0f8d21d36a507106ba5b6172fc23203a993fd581
m0lc14kk <m0lc14kk@outlook.com>
2024-11-27 18:58:26 +0100

    feat: some changes for FAQ blocks

--- src/components/pages/about/QuestionComponent.tsx
+++ src/components/pages/about/QuestionComponent.tsx
@@ -4,18 +4,21 @@ import type { QuestionComponentType } from "./types/QuestionComponentType";
 
 const QuestionComponent = ({ question, answer }: QuestionComponentType) => {
     const [isToggled, setToggled] = useState<boolean>(false);
+    const handleClick = (): void => {
+        setToggled(!isToggled);
+    };
     
     return (
         <>
             <article className="bg-transparent flex flex-col">
-                <div className="w-full p-6 border-l-[3px] bg-[#101010] border-l-[#FF55FF] flex justify-between items-center cursor-pointer" onClick={(): void => setToggled(!isToggled)}>
+                <div className="w-full p-6 border-l-[3px] bg-[#101010] border-l-[#FF55FF] flex justify-between items-center cursor-pointer" onClick={handleClick}>
                     <h1 className="bg-transparent text-xl">
                         {question}
                     </h1>
 
                     <IoIosArrowDown
                         size={25}
-                        className={`bg-transparent duration-150 ${isToggled ? "-rotate-90" : "rotate-0"}`}
+                        className={`bg-transparent duration-150 ${isToggled ? "-rotate-180" : "rotate-0"}`}
                     />
                 </div>
 
