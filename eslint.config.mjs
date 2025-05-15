9a438d082f00e63078a135d7da1a5e9bb977e2de
m0lc14kk <m0lc14kk@outlook.com>
2025-05-15 19:57:44 +0200

    style(prettier): formatted entire project

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,21 +1,21 @@
-import js from "@eslint/js";
-import globals from "globals";
-import tseslint from "typescript-eslint";
-import json from "@eslint/json";
-import { defineConfig } from "eslint/config";
+import js from "@eslint/js"
+import globals from "globals"
+import tseslint from "typescript-eslint"
+import json from "@eslint/json"
+import { defineConfig } from "eslint/config"
 
 export default defineConfig([
-  tseslint.configs.recommended,
-  {
-    files: ["**/*.{js,mjs,cjs,ts}"],
-    plugins: { js },
-    languageOptions: { globals: globals.node },
-    rules: {
-      "@typescript-eslint/no-unused-vars": ["error", { argsIgnorePattern: "^_$" }],
+    tseslint.configs.recommended,
+    {
+        files: ["**/*.{js,mjs,cjs,ts}"],
+        plugins: { js },
+        languageOptions: { globals: globals.node },
+        rules: {
+            "@typescript-eslint/no-unused-vars": ["error", { argsIgnorePattern: "^_$" }],
+        },
+        extends: ["js/recommended"],
     },
-    extends: ["js/recommended"],
-  },
-  { files: ["**/*.json"], plugins: { json }, language: "json/json", extends: ["json/recommended"] },
-  { files: ["**/*.jsonc"], plugins: { json }, language: "json/jsonc", extends: ["json/recommended"] },
-  { files: ["**/*.json5"], plugins: { json }, language: "json/json5", extends: ["json/recommended"] },
-]);
+    { files: ["**/*.json"], plugins: { json }, language: "json/json", extends: ["json/recommended"] },
+    { files: ["**/*.jsonc"], plugins: { json }, language: "json/jsonc", extends: ["json/recommended"] },
+    { files: ["**/*.json5"], plugins: { json }, language: "json/json5", extends: ["json/recommended"] },
+])
