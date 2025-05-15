855c9f8e75bc1b4ccbefc08aa3450c6b8b1eb946
m0lc14kk <m0lc14kk@outlook.com>
2025-05-15 19:59:44 +0200

    feat(dev): added main file

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+/**
+ * WEBHOOK API
+ * 
+ * Webhook API is a library that allows developers to easily send
+ * their webhooks from Minecraft Vanilla server, without having to
+ * struggle with entire set-up of HTTP.
+ * 
+ * @author m0lc14kk
+ * @license Apache-2.0
+ */
