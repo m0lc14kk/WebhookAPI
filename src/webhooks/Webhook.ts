9063a5e546ef1f362f57560a223b703ccadb0ba4
m0lc14kk <m0lc14kk@outlook.com>
2025-05-16 10:11:59 +0200

    feat(webhooks): implemented URL validation and getWebhook method

--- src/webhooks/Webhook.ts
+++ src/webhooks/Webhook.ts
@@ -1,11 +1,30 @@
-class Webhook {
-    public webhookUri: string
+import { http, HttpResponse } from "@minecraft/server-net"
+import type { WebhookTypeUnion } from "./types/WebhookTypeUnion"
 
-    public constructor(webhookUri: string) {
-        this.webhookUri = webhookUri
+class Webhook {
+    public static validateUri(webhookUrl: string): boolean {
+        return /^https:\/\/(canary\.|ptb\.)?discord\.com\/api\/webhooks\/\d{17,20}\/[A-Za-z0-9_-]{60,70}$/.test(webhookUrl)
     }
 
-    public async getWebhook() {}
+    public webhookUrl: string
+
+    public constructor(webhookUrl: string) {
+        if (Webhook.validateUri(webhookUrl)) {
+            throw new Error("DataError: Invalid webhook's URL has been provided.")
+        }
+
+        this.webhookUrl = webhookUrl
+    }
+
+    public async getWebhook<T extends WebhookTypeUnion>(): Promise<T | null> {
+        try {
+            const request: HttpResponse = await http.get(this.webhookUrl)
+            if (request.status === 200) return JSON.parse(request.body)
+            return null
+        } catch {
+            return null
+        }
+    }
 
     public async editWebhook() {}
 
---
+++ src/webhooks/types/WebhookTypeUnion.ts
@@ -0,0 +1,7 @@
+import type { IWebhookApplicationStructure } from "../interfaces/IWebhookApplicationStructure";
+import type { IWebhookChannelFollowerStructure } from "../interfaces/IWebhookChannelFollowerStructure";
+import type { IWebhookIncomingStructure } from "../interfaces/IWebhookIncomingStructure";
+
+type WebhookTypeUnion = IWebhookApplicationStructure | IWebhookChannelFollowerStructure | IWebhookIncomingStructure
+
+export type { WebhookTypeUnion }
