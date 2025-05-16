b9fd7c3e16846c7d090d520bfde874db52aeed88
m0lc14kk <m0lc14kk@outlook.com>
2025-05-16 21:24:10 +0200

    feat(types): added component types

--- src/components/types/ComponentType.ts
+++ src/components/types/ComponentType.ts
@@ -1,5 +1,20 @@
 /* eslint-disable no-unused-vars */
 
-enum ComponentType {}
+enum ComponentType {
+    ACTION_ROW = 1,
+    BUTTON = 2,
+    STRING_SELECT = 3,
+    USER_SELECT = 5,
+    ROLE_SELECT = 6,
+    MENTIONABLE_SELECT = 7,
+    CHANNEL_SELECT = 8,
+    SECTION = 9,
+    TEXT_DISPLAY = 10,
+    THUMBNAIL = 11,
+    MEDIA_GALERY = 12,
+    FILE = 13,
+    SEPARATOR = 14,
+    CONTAINER = 17,
+}
 
 export { ComponentType }
