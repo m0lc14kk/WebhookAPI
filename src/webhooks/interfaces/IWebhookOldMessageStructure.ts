a0a5db151a5737da7a920e53341226523a6bc561
m0lc14kk <m0lc14kk@outlook.com>
2025-05-16 17:50:32 +0200

    fix(schema): fixed old message schema

--- src/webhooks/interfaces/IWebhookOldMessageStructure.ts
+++ src/webhooks/interfaces/IWebhookOldMessageStructure.ts
@@ -1,8 +1,22 @@
+import { EmbedBuilder } from "../../components/old/embeds/EmbedBuilder"
+import { PollBuilder } from "../../components/old/polls/PollBuilder"
 import { WebhookMessageType } from "../constants/WebhookMessageType"
 import { IWebhookMessageStructure } from "./IWebhookMessageStructure"
 
 type IWebhookOldMessageStructure = IWebhookMessageStructure & {
     version: WebhookMessageType.OLD
-}
+} & ({
+    content: string
+    embeds?: EmbedBuilder[]
+    poll?: PollBuilder
+} | {
+    content?: string
+    embeds: EmbedBuilder[]
+    poll?: PollBuilder
+} | {
+    content?: string
+    embeds?: EmbedBuilder[]
+    poll: PollBuilder
+})
 
 export { IWebhookOldMessageStructure }
