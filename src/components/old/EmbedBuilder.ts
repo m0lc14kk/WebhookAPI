3aa13e4aa685e960c2155c53fdf159bbec6e62b1
m0lc14kk <m0lc14kk@outlook.com>
2025-05-16 11:02:44 +0200

    feat(embeds): added old embeds constructor

---
+++ src/components/old/EmbedBuilder.ts
@@ -0,0 +1,34 @@
+class EmbedBuilder {
+    private title: string | null = null
+    private description: string | null = null
+    private color: number = 0x000000
+
+    public constructor() {
+
+    }
+
+    public setTitle(title: string): EmbedBuilder {
+        this.title = title
+        return this
+    }
+
+    public setDescription(description: string | string[]): EmbedBuilder {
+        this.description = Array.isArray(description) ? description.join("\n") : description
+        return this
+    }
+
+    public setColor(color: string | number): EmbedBuilder {
+        this.color = typeof color === "string" ? +color.slice(1) : color
+        return this
+    }
+
+    public toJSON() {
+        return {
+            title: this.title,
+            description: this.description,
+            color: this.color
+        }
+    }
+}
+
+export { EmbedBuilder }
