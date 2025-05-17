80a2451a1b8f5577845630fcbb3902f8734401f5
m0lc14kk <m0lc14kk@outlook.com>
2025-05-17 16:01:30 +0200

    feat(components): implemented thumbnail component

---
+++ src/components/new/display/ThumbnailComponent.ts
@@ -0,0 +1,37 @@
+import { Component } from "../../Component";
+import { ComponentType } from "../../constants/ComponentType";
+
+class ThumbnailComponent extends Component {
+    public static override readonly type: ComponentType = ComponentType.THUMBNAIL;
+    private url: string | null = null
+    private description: string | null = null
+    private spoiler: boolean = false
+
+    public setUrl(url: string): this {
+        this.url = url
+        return this
+    }
+
+    public setDescription(description: string): this {
+        this.description = description
+        return this
+    }
+
+    public setSpoiler(spoiler: boolean): this {
+        this.spoiler = spoiler
+        return this
+    }
+
+    public toJSON() {
+        return {
+            type: ThumbnailComponent.type,
+            description: this.description,
+            spoiler: this.spoiler,
+            media: {
+                url: this.url
+            }
+        }
+    }
+}
+
+export { ThumbnailComponent }
