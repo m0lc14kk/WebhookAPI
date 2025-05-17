d9a463c968bf278fc9ba3259b3145e24b2689f7b
m0lc14kk <m0lc14kk@outlook.com>
2025-05-17 08:38:45 +0200

    feat(security): added Snowflake identifier validation

--- src/components/types/ComponentEmojiType.ts
+++ src/components/types/ComponentEmojiType.ts
@@ -1,11 +1,11 @@
 type ComponentEmojiType =
     | {
-        id?: string
-        name: string
-    }
+          id?: string
+          name: string
+      }
     | {
-        id: string
-        name?: string
-    }
+          id: string
+          name?: string
+      }
 
 export type { ComponentEmojiType }
---
+++ src/validators/SnowflakeValidator.ts
@@ -0,0 +1,20 @@
+const SNOWFLAKE_REGEXP: Readonly<RegExp> = /^\d{17,19}$/
+const discordEpoch: bigint = 1420070400000n;
+
+class SnowflakeValidator {
+    private constructor() { }
+
+    public static isSnowflake(snowflake: unknown): snowflake is string {
+        if (typeof snowflake !== "string" || !SNOWFLAKE_REGEXP.test(snowflake)) return false
+
+        try {
+            const snowflakeBigInt: bigint = BigInt(snowflake);
+            const timestamp = Number((snowflakeBigInt >> 22n) + discordEpoch)
+            return timestamp > 1420070400000 && timestamp < Date.now() + 5 * 60 * 1000
+        } catch {
+            return false
+        }
+    }
+}
+
+export { SnowflakeValidator }
