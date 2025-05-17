5a864b04c47481486ae7ceb0bcde0a7768b9ab6f
m0lc14kk <m0lc14kk@outlook.com>
2025-05-17 16:27:14 +0200

    feat(components): added file component

---
+++ src/components/new/media/FileComponent.ts
@@ -0,0 +1,32 @@
+import { Component } from "../../Component";
+import { ComponentType } from "../../constants/ComponentType";
+
+class FileComponent extends Component {
+    public static override readonly type: ComponentType = ComponentType.FILE;
+    private fileUrl: string | null = null
+    private spolier: boolean = false
+
+    public setFileUrl(fileUrl: string): this {
+        this.fileUrl = fileUrl
+        return this        
+    }
+
+    public setSpoiler(spoiler: boolean): this {
+        this.spolier = spoiler
+        return this
+    }
+
+    public toJSON() {
+        if (this.fileUrl === null) throw new Error("DataError: You must provide file's URL to create a file component.")
+
+        return {
+            type: FileComponent.type,
+            file: {
+                url: this.fileUrl
+            },
+            spoiler: this.spolier
+        }
+    }
+}
+
+export { FileComponent }
--- src/components/new/media/MediaGalleryComponent.ts
+++ src/components/new/media/MediaGalleryComponent.ts
@@ -1,10 +1,10 @@
-import { Component } from "../../Component";
-import { ComponentType } from "../../constants/ComponentType";
-import { IMediaGalleryItemStructure } from "./interfaces/IMediaGalleryItemStructure";
+import { Component } from "../../Component"
+import { ComponentType } from "../../constants/ComponentType"
+import { IMediaGalleryItemStructure } from "./interfaces/IMediaGalleryItemStructure"
 
 class MediaGalleryComponent extends Component {
-    public static override readonly type: ComponentType = ComponentType.MEDIA_GALERY;
-    private images: IMediaGalleryItemStructure[] = [] 
+    public static override readonly type: ComponentType = ComponentType.MEDIA_GALERY
+    private images: IMediaGalleryItemStructure[] = []
 
     public addImages(...images: IMediaGalleryItemStructure[]): this {
         this.images.push(...images)
@@ -19,7 +19,7 @@ class MediaGalleryComponent extends Component {
     public toJSON() {
         return {
             type: MediaGalleryComponent.type,
-            items: this.images
+            items: this.images,
         }
     }
 }
