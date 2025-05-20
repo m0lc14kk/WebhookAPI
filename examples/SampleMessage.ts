5729e5e25feb2167b4a126f241f864ae9a914a6e
m0lc14kk <m0lc14kk@outlook.com>
2025-05-20 08:48:51 +0200

    feat(examples): added example of an API

---
+++ examples/SampleMessage.ts
@@ -0,0 +1,33 @@
+import { world } from "@minecraft/server"
+import { ContainerComponent, MediaGalleryComponent, TextDisplayComponent, Webhook, WebhookMessageType } from "../src" // Change path to your's index file.
+
+// Change Webhook's URL.
+const WEBHOOK_URL = "YOUR_WEBHOOK_URL"
+const webhook: Webhook = new Webhook(WEBHOOK_URL)
+
+world.afterEvents.worldLoad.subscribe((): void => {
+    webhook.sendMessage({
+        version: WebhookMessageType.NEW,
+        components: [
+            new ContainerComponent()
+                .setAccentColor(null)
+                .setComponents(
+                    new TextDisplayComponent()
+                        .setContent([
+                            "# 🔥 Webhook API",
+                            "Thanks for using my API in your project. This is a sample code, that will be sent after the world loads, if an API is set-up correctly. If you see this message on your channel, congrats!",
+                            "",
+                            "- GitHub: [Webhook API](https://github.com/m0lc14kk/WebhookAPI)"
+                        ]),
+                    new MediaGalleryComponent()
+                        .setImages({
+                            media: {
+                                url: "https://github.com/m0lc14kk/WebhookAPI/blob/main/.github/assets/banner.png?raw=true"
+                            }
+                        })
+                )
+        ]
+    }, {
+        withComponents: true
+    })
+})
