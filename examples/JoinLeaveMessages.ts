df769e5863b54ca450ff3a9b81381a3986eabc02
m0lc14kk <m0lc14kk@outlook.com>
2025-05-25 13:28:19 +0200

    feat(examples): added new example

---
+++ examples/JoinLeaveMessages.ts
@@ -0,0 +1,42 @@
+/**
+ * This example is a sample code, that will log every player's join
+ * and leave to a Minecraft server as themselves.
+ */
+
+import { PlayerJoinAfterEvent, PlayerLeaveBeforeEvent, system, world } from "@minecraft/server"
+import { TextDisplayComponent, Webhook, WebhookMessageType } from "../src" // Change path to your's index file.
+
+// Change Webhook's URL.
+const WEBHOOK_URL: string = "YOUR_WEBHOOK_URL"
+const webhook: Webhook = new Webhook(WEBHOOK_URL)
+
+world.afterEvents.playerJoin.subscribe(({ playerName }: PlayerJoinAfterEvent): void => {
+    system.run((): void => {
+        webhook.sendMessage({
+            version: WebhookMessageType.NEW,
+            username: playerName,
+            components: [
+                new TextDisplayComponent()
+                    .setContent("I just joined a server!")
+            ]
+        })
+    })
+})
+
+world.beforeEvents.playerLeave.subscribe(({ player }: PlayerLeaveBeforeEvent): void => {
+    /**
+     * You must save player's name, beacuse in system.run player will be undefined
+     * due to privilege system.
+     */
+    const { name: playerName } = player
+    system.run((): void => {
+        webhook.sendMessage({
+            version: WebhookMessageType.NEW,
+            username: playerName,
+            components: [
+                new TextDisplayComponent()
+                    .setContent("I just left a server!")
+            ]
+        })
+    })
+})
