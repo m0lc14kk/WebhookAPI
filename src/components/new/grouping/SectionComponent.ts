9315e434979189de6d27bae9ad18288901d41e1a
m0lc14kk <m0lc14kk@outlook.com>
2025-05-27 12:45:09 +0200

    feat(security): added validation for section component

--- src/components/new/grouping/SectionComponent.ts
+++ src/components/new/grouping/SectionComponent.ts
@@ -2,6 +2,9 @@ import { Component } from "../../Component"
 import { ComponentType } from "../../constants/ComponentType"
 import type { SectionComponentTypes } from "./types/SectionComponentTypes"
 import { SectionAccessoryTypes } from "./types/SectionAccessoryTypes"
+import { TextDisplayComponent } from "../display/TextDisplayComponent"
+import { ThumbnailComponent } from "../display/ThumbnailComponent"
+import { BaseButtonComponent } from "../buttons/BaseButtonComponent"
 
 class SectionComponent extends Component {
     public static override readonly type: ComponentType = ComponentType.SECTION
@@ -12,8 +15,10 @@ class SectionComponent extends Component {
      * Sets new components to this section.
      * @param components New components.
      * @returns Edited instance.
+     * @throws Throws an error if one of provided components is invalid.
      */
     public setComponents(...components: SectionComponentTypes[]): this {
+        this.validateComponents(components)
         this.components = components
         return this
     }
@@ -22,8 +27,10 @@ class SectionComponent extends Component {
      * Adds new components to existing ones.
      * @param components New components.
      * @returns Edited instance.
+     * @throws Throws an error if one of provided components is invalid.
      */
     public addComponents(...components: SectionComponentTypes[]): this {
+        this.validateComponents(components)
         this.components.push(...components)
         return this
     }
@@ -32,10 +39,19 @@ class SectionComponent extends Component {
      * Sets an accessory of a section.
      * @param accessory New accessory.
      * @returns Edited instance.
+     * @throws Throws an error if you provided invalid accessory.
      */
     public setAccessory(accessory: SectionAccessoryTypes): this {
+        if (!(accessory instanceof ThumbnailComponent) && !(accessory instanceof BaseButtonComponent)) throw new TypeError("TypeError: Section's accessory must be a button or a thumbnail.")
         this.accessory = accessory
         return this
+    }
+
+    private validateComponents(components: SectionComponentTypes[]): void {
+        for (const component of components) {
+            if (component instanceof TextDisplayComponent) continue
+            throw new TypeError("TypeError: Invalid section's component.")
+        }
     }
 
     /**
