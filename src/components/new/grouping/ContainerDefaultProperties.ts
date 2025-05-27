fdf9349dbd3085cc637c21b7c4d05486035fe50d
m0lc14kk <m0lc14kk@outlook.com>
2025-05-27 14:06:00 +0200

    feat(config->default): added validation for container default properties

--- src/components/new/grouping/ContainerDefaultProperties.ts
+++ src/components/new/grouping/ContainerDefaultProperties.ts
@@ -4,12 +4,13 @@ import { MAX_COLOR_NUMBER } from "../../../globals"
  * A class to manage default properties of containers.
  */
 class ContainerDefaultProperties {
-    private constructor() {}
+    private constructor() { }
     private static defaultContainerAccentColor: number | null = 0x000000
 
     /**
      * Sets a default color for all containers.
      * @param defaultAccentColor Default color of containers.
+     * @throws Throws an error if provided color is invalid.
      */
     public static setDefaultContainerAccentColor(defaultAccentColor: string | number | null): void {
         if (defaultAccentColor === null) {
@@ -21,9 +22,10 @@ class ContainerDefaultProperties {
             if (!defaultAccentColor.startsWith("#") && defaultAccentColor.length !== 7) throw new Error("DataError: Invalid color HEX.")
             this.defaultContainerAccentColor = Number(`0x${defaultAccentColor.slice(1)}`)
             return
-        }
+        } else if (typeof defaultAccentColor === "number") {
+            if (defaultAccentColor > MAX_COLOR_NUMBER || defaultAccentColor < 0) throw new Error("DataError: Color is out of bounds.")
+        } else throw new TypeError("TypeError: Invalid default accent color.")
 
-        if (defaultAccentColor > MAX_COLOR_NUMBER || defaultAccentColor < 0) throw new Error("DataError: Color is out of bounds.")
         this.defaultContainerAccentColor = Math.floor(defaultAccentColor)
     }
 
