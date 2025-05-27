90884663ed5713ad20839c793181c6c72056b523
m0lc14kk <m0lc14kk@outlook.com>
2025-05-27 11:54:00 +0200

    feat(docs): documented emoji validation

--- src/validators/DiscordEmojiValidator.ts
+++ src/validators/DiscordEmojiValidator.ts
@@ -1,17 +1,25 @@
-import { ComponentEmojiType } from "../components/types/ComponentEmojiType";
-import { SnowflakeValidator } from "./SnowflakeValidator";
+import { ComponentEmojiType } from "../components/types/ComponentEmojiType"
+import { SnowflakeValidator } from "./SnowflakeValidator"
 
+/**
+ * A class to validate Discord emojis.
+ */
 class DiscordEmojiValidator {
     private constructor() {}
 
-    public static validateEmoji(emoji: string | ComponentEmojiType): emoji is string | ComponentEmojiType {
+    /**
+     * Validates Discord message.
+     * @param emoji Emoji to validate.
+     * @throws Throws an error, if there is something invalid in this emoji.
+     */
+    public static validateEmoji(emoji: string | ComponentEmojiType): void {
         if (typeof emoji === "string") {
-            return true
+            return
         } else if (typeof emoji === "object") {
             if (!emoji.name && !emoji.id) throw new Error("DataError: You have to provide an identifier or a name of an emoji.")
             if (emoji.id && !SnowflakeValidator.isSnowflake(emoji.id)) throw new Error("DataError: Invalid emoji's identifier.")
         }
-        
+
         throw new TypeError("TypeError: Invalid emoji type.")
     }
 }
