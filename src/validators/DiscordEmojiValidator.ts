23f42c804d83118257daea9fe1858e6e51ddb3b7
m0lc14kk <m0lc14kk@outlook.com>
2025-05-27 16:11:31 +0200

    feat(security->emojis): added validation for `animated` field

--- src/validators/DiscordEmojiValidator.ts
+++ src/validators/DiscordEmojiValidator.ts
@@ -1,4 +1,4 @@
-import { ComponentEmojiType } from "../components/types/ComponentEmojiType"
+import type { ComponentEmojiAnimatedType } from "../components/types/ComponentEmojiAnimatedType"
 import { SnowflakeValidator } from "./SnowflakeValidator"
 
 /**
@@ -12,12 +12,13 @@ class DiscordEmojiValidator {
      * @param emoji Emoji to validate.
      * @throws Throws an error, if there is something invalid in this emoji.
      */
-    public static validateEmoji(emoji: string | ComponentEmojiType): void {
+    public static validateEmoji(emoji: string | ComponentEmojiAnimatedType): void {
         if (typeof emoji === "string") {
             return
         } else if (typeof emoji === "object") {
             if (!emoji.name && !emoji.id) throw new Error("DataError: You have to provide an identifier or a name of an emoji.")
             if (emoji.id && !SnowflakeValidator.isSnowflake(emoji.id)) throw new Error("DataError: Invalid emoji's identifier.")
+            if (typeof (emoji.animated ?? false) !== "boolean") throw new TypeError("TypeError: Invalid emoji's animation state.")
         }
 
         throw new TypeError("TypeError: Invalid emoji type.")
