d18a99df29f4c26d8ef8fe90d89daa0a337c1059
m0lc14kk <m0lc14kk@outlook.com>
2025-05-27 14:18:33 +0200

    feat(docs): documented button constants

--- src/components/new/buttons/constants/ButtonComponentAllowedStyles.ts
+++ src/components/new/buttons/constants/ButtonComponentAllowedStyles.ts
@@ -1,5 +1,8 @@
 import { ButtonStyle } from "./ButtonStyle"
 
+/**
+ * Set of allowed styles for buttons.
+ */
 const BUTTON_COMPONENT_ALLOWED_STYLES: Readonly<Set<ButtonStyle>> = new Set<ButtonStyle>([ButtonStyle.PRIMARY, ButtonStyle.SECONDARY, ButtonStyle.SUCCESS, ButtonStyle.DANGER])
 
 export { BUTTON_COMPONENT_ALLOWED_STYLES }
--- src/components/new/buttons/constants/LinkButtonComponentAllowedUrlProtocols.ts
+++ src/components/new/buttons/constants/LinkButtonComponentAllowedUrlProtocols.ts
@@ -1,3 +1,6 @@
+/**
+ * A set of allowed URL protocols for link buttons.
+ */
 const LINK_BUTTON_COMPONENT_ALLOWED_URL_PROTOCOLS: Readonly<Set<string>> = new Set<string>(["discord://", "https://", "http://"])
 
 export { LINK_BUTTON_COMPONENT_ALLOWED_URL_PROTOCOLS }
