30285a89c4bbc193882167615cd83c8cb028a6b2
m0lc14kk <m0lc14kk@outlook.com>
2025-06-20 14:41:01 +0200

    fix(prettier): changed file name `.prettierrc` -> `.prettierrc.json`

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,7 @@
 import js from "@eslint/js"
+import json from "@eslint/json"
 import globals from "globals"
 import tseslint from "typescript-eslint"
-import json from "@eslint/json"
 import { defineConfig } from "eslint/config"
 
 export default defineConfig([
@@ -12,32 +12,35 @@ export default defineConfig([
     {
         files: ["**/*.{js,mjs,cjs,ts}"],
         plugins: { js },
-        languageOptions: { 
-            globals: globals.node 
+        languageOptions: {
+            globals: globals.node,
         },
         rules: {
-            "@typescript-eslint/no-unused-vars": ["error", { 
-                argsIgnorePattern: "^_$" 
-            }],
+            "@typescript-eslint/no-unused-vars": [
+                "error",
+                {
+                    argsIgnorePattern: "^_$",
+                },
+            ],
         },
         extends: ["js/recommended"],
     },
-    { 
-        files: ["**/*.json"], 
-        plugins: { json }, 
-        language: "json/json", 
-        extends: ["json/recommended"] 
+    {
+        files: ["**/*.json"],
+        plugins: { json },
+        language: "json/json",
+        extends: ["json/recommended"],
     },
-    { 
-        files: ["**/*.jsonc"], 
-        plugins: { json }, 
-        language: "json/jsonc", 
-        extends: ["json/recommended"] 
+    {
+        files: ["**/*.jsonc"],
+        plugins: { json },
+        language: "json/jsonc",
+        extends: ["json/recommended"],
     },
-    { 
-        files: ["**/*.json5"], 
-        plugins: { json }, 
-        language: "json/json5", 
-        extends: ["json/recommended"] 
+    {
+        files: ["**/*.json5"],
+        plugins: { json },
+        language: "json/json5",
+        extends: ["json/recommended"],
     },
 ])
