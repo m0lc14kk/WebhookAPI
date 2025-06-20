216e8a93932469baea438138bacbd10c80cf64b6
m0lc14kk <m0lc14kk@outlook.com>
2025-06-20 14:39:40 +0200

    style(prettier): formatted ESLint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -12,13 +12,32 @@ export default defineConfig([
     {
         files: ["**/*.{js,mjs,cjs,ts}"],
         plugins: { js },
-        languageOptions: { globals: globals.node },
+        languageOptions: { 
+            globals: globals.node 
+        },
         rules: {
-            "@typescript-eslint/no-unused-vars": ["error", { argsIgnorePattern: "^_$" }],
+            "@typescript-eslint/no-unused-vars": ["error", { 
+                argsIgnorePattern: "^_$" 
+            }],
         },
         extends: ["js/recommended"],
     },
-    { files: ["**/*.json"], plugins: { json }, language: "json/json", extends: ["json/recommended"] },
-    { files: ["**/*.jsonc"], plugins: { json }, language: "json/jsonc", extends: ["json/recommended"] },
-    { files: ["**/*.json5"], plugins: { json }, language: "json/json5", extends: ["json/recommended"] },
+    { 
+        files: ["**/*.json"], 
+        plugins: { json }, 
+        language: "json/json", 
+        extends: ["json/recommended"] 
+    },
+    { 
+        files: ["**/*.jsonc"], 
+        plugins: { json }, 
+        language: "json/jsonc", 
+        extends: ["json/recommended"] 
+    },
+    { 
+        files: ["**/*.json5"], 
+        plugins: { json }, 
+        language: "json/json5", 
+        extends: ["json/recommended"] 
+    },
 ])
